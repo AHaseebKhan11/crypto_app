1f9a740a31447694b2c705413c54519d79f1fe8e
Haseeb Khan <haseeb.khn@hotmail.com>
2018-05-30 00:27:29 +0500

    Coin graphs default update

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,7 +5,7 @@ class Post < ActiveRecord::Base
   validates :user_id, presence: true
   validates :content, presence: true, length: { maximum: 140 } # tweets are capped at 140 chars.
   default_scope -> { order(created_at: :desc) } # newest tweets / posts first
-  cattr_accessor :present_values
+  cattr_accessor :present_values, :present_values_actual
   scoped_search on: [:content]
 
   @@present_values = ['$BTC',
@@ -28,4 +28,24 @@ class Post < ActiveRecord::Base
                       '$HT',
                       '$XMR',
                       '$ZEC']
+  @@present_values_actual = ['BTC',
+                              'ETH',
+                              'EOS',
+                              'BCH',
+                              'TRX',
+                              'LTC',
+                              'XRP',
+                              'ETC',
+                              'CTXC',
+                              'DASH',
+                              'ADA',
+                              'ONT',
+                              'IOT',
+                              'NEO',
+                              'XLM',
+                              'QTUM',
+                              'BNB',
+                              'HT',
+                              'XMR',
+                              'ZEC']
 end
