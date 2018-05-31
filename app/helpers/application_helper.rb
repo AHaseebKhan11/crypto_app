2fc5c4593ba06823700bccbb73e2f9196427b91b
Haseeb Khan <haseeb.khn@hotmail.com>
2018-05-31 23:46:56 +0500

    Fix  links

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,7 +3,7 @@ module ApplicationHelper
     content_words = content.split(" ")
     content_with_links = content_words.map do |word|
       if Post.present_values.include? word
-        link_to word, "/graphs?type=#{word.reverse.chop.reverse.downcase}"
+        link_to word, "/coins?type=#{word.reverse.chop.reverse.downcase}"
       else
         word
       end
