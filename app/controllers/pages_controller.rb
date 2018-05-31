78a630127dc281d57360cd985fce872228bf67e7
Haseeb Khan <haseeb.khn@hotmail.com>
2018-06-01 02:42:36 +0500

    Follow users

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -19,13 +19,14 @@ class PagesController < ApplicationController
   def profile
     # grab the username from the URL as :id
     if (User.find_by_username(params[:id]))
+      puts 'x'*400
       @username = params[:id]
+      @posts = Post.all.where("user_id = ?", User.find_by_username(params[:id]).id)
     else
       # redirect to 404 (root for now)
       redirect_to root_path, :notice=> "User not found!"
     end
 
-    @posts = Post.all.where("user_id = ?", User.find_by_username(params[:id]).id)
   end
 
   # back-end code for pages/explore
--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -1,15 +1,16 @@
 class RelationshipsController < ApplicationController
-    
+
     def create
         user = User.find(params[:followed_id])
         current_user.follow(user)
-        redirect_to(:back)
+        redirect_to "/user/#{user.username}"
     end
-    
+
     def destroy
-        user = Relationship.find(params[:id]).followed 
+        # user = Relationship.find(params[:id]).followed
+        user = User.find(params[:id])
         current_user.unfollow(user)
-        redirect_to(:back)
+        redirect_to "/user/#{user.username}"
     end
-    
+
 end
